08e85eb59f2d679310983c2259702325359b4824
Fazulhaq Amin <amin.jawan@yahoo.com>
2024-01-18 09:48:11 +0430

    Get data from graphql by passing title in query

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,20 +1,42 @@
-import { Fragment, useContext, useEffect, useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { Fragment, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import ProductCard from '../../components/product-card/product-card.component';
 
 import Spinner from "../../components/spinner/spinner.component";
-import { CategoriesContext } from '../../contexts/categories.context';
 import { CategoryContainer, Title } from './category.styles';
+
+const GET_CATEGORY = gql`
+  query($title: String) {
+    getCollectionsByTitle(title: $title){
+    id
+    title
+    items {
+      id
+      name
+      price
+      imageUrl
+    }
+  }
+  }
+`;
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap, loading } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
-
+  const [products, setProducts] = useState([]);
+  const {loading, error, data } = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category,
+    }
+  });
+  
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if(data){
+      const { getCollectionsByTitle: { items }} = data;
+      setProducts(items);
+    }
+  },[category, data]);
 
   return (
     <Fragment>
